b0800b8ed04fbb12549025f6c5bb7cafec368ae7
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-09-10 08:52:47 -0300

    restile: ProductForm.styled.ts

--- src/components/ProductForm/ProductForm/ProductForm.styled.ts
+++ src/components/ProductForm/ProductForm/ProductForm.styled.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { Form } from 'react-bootstrap';
 
 export const FormStyled = styled(Form)`
-    padding: 1rem;
-    border: 1px solid #99a1af;
-    border-radius: 5px;
+  padding: 1rem;
+  border: 1px solid #99a1af;
+  border-radius: 5px;
 
   .form-group {
     margin-bottom: 0.25rem;
