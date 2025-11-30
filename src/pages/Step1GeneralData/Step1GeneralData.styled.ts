91172f42bcd5c6ff457aa3f621ce7d579965569f
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-30 17:35:49 -0300

    restyling: Step1GeneralData colors

--- src/pages/Step1GeneralData/Step1GeneralData.styled.ts
+++ src/pages/Step1GeneralData/Step1GeneralData.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { Colors } from '../../constants/colors';
 
 export const DivStyled = styled.div`
-    border: 1px solid rgba(153, 161, 175, 1);
+    border: 1px solid ${Colors.black};
     border-radius: 5px;
 `;
