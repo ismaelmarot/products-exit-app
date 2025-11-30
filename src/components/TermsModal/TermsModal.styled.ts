cc2c0998cdf4e33490573d4f3ed195401c894fc9
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-30 17:32:00 -0300

    restyling: TermsMOdal.styled.tsx

--- src/components/TermsModal/TermsModal.styled.ts
+++ src/components/TermsModal/TermsModal.styled.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { Modal } from 'react-bootstrap';
+import { Colors } from '../../constants/colors';
 
 export const StyledModalBody = styled(Modal.Body)`
     font-size: 0.7rem;
     text-align: justify;
     line-height: 1.4;
-    color: #333;
+    color: ${Colors.black};
 `;
 
 export const LinkStyled = styled.a`
